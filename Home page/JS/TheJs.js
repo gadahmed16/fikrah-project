6e112e955e791fc3c7bd504bc65f78c67b364cfa
unknown <you@example.com>
2023-12-27 21:59:24 +0200

    V1.1

--- Home page/JS/TheJs.js
+++ Home page/JS/TheJs.js
@@ -10,3 +10,23 @@ setTimeout(() => {
 setTimeout(() => {
     TheIntroPage.style = "display:none;";
 }, 2300);
+// TheSeachBarActive 
+let TheSearchBarActiveComponts = [...document.getElementsByClassName("ActiveOnClick")];
+let ExitMark = document.getElementsByClassName("ssss")[0];
+TheSearchBarActiveComponts.forEach(Element => {
+    Element.addEventListener("click",() => {
+        TheSearchBarActiveComponts[2].focus();
+        TheSearchBarActiveComponts[0].style.transition = " all .2s linear";
+        TheSearchBarActiveComponts[0].style.border = "2px solid black";
+        TheSearchBarActiveComponts[0].style.width = "300px";
+        ExitMark.style.display = "block ";
+    })
+})
+ExitMark.addEventListener("click",() => {
+    TheSearchBarActiveComponts[2].blur();
+    TheSearchBarActiveComponts[0].style.transition = " all .1s linear";
+    TheSearchBarActiveComponts[0].style.border = "0px solid black";
+    TheSearchBarActiveComponts[0].style.width = "240px";
+    ExitMark.style.display = "none";
+    TheSearchBarActiveComponts[2].value = "";
+})
